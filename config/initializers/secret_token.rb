be932f6d720cfec6f0899988c3c5bcf5bf53dfe0
Nina Andersen <nina@vimond.com>
2013-10-25 11:31:39 +0200

    Adding the whole project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mod250prosjekt::Application.config.secret_token = '7ba16a260ef303482a159823965007afc2563895a13130caebb90bfa240154c62df0864d3494923dd10c3617ade6c69faba3ae9c3d1139e808e849c895d77220'
